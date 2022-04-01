cf1d4472d08fc9f6617cf716dbd83b560d3930e2
wgf4242 <wgf4242@gmail.com>
2022-04-01 12:55:06 +0800

    15丨实战痛点1：复杂Vue项目的规范和基础库封装 添加Element3,Eslint 移除Todo示例,

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -161,3 +161,23 @@ function removeTodo(e,i){
   right: 0;top: 0;
 }
 </style>
+
+<style lang="scss" scoped>
+$padding:10px;
+$white:#fff;
+ul {
+  width:500px;
+  margin:0 auto;
+  padding: 0;
+  li {
+    &:hover {
+      cursor: pointer;
+    }
+    list-style-type: none;
+    margin-bottom: $padding;
+    padding: $padding;
+    background: $white;
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
